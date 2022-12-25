a397aa2f6304f25a8b08c24ac793854fc1d3440b
jkhdr <redhydratechnologies@gmail.com>
2022-12-24 19:31:43 -0500

    readme

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,35 @@ using namespace std;
 
 ///////
 
+void print_tick(int i){cout << pdt.tick << i;}
+
+struct printData{  // to remove printing to increasing efficiency
+
+  short completePercent; 
+
+  string m1ok = "Servo m1: OK";
+  string m1er = "Servo m1: ERR";
+
+  string m2ok = "Servo m2:" "OK";
+  string m2er = "Servo m2:" "ERR";
+
+  string sb = "Booting Servo m";
+
+  string lok = "RPLidar l: OK";
+  string ler = "RPLidar l: ERR";
+  string lb = "Booting RPLidar...";
+
+  string SETUP = "SETUP ///////";
+  string b = "booting";
+
+  string dotdotdot = "...";
+
+  string tick = "Tick #";
+};
+
+
+//////
+
 class Hub{
   public:
     const int Pin_l = pin_L; 
@@ -26,51 +55,32 @@ void boot_hub(Hub h){
   //TODO object configuration
 }
 
-ThreadController controll = ThreadController();
-/*
-//main protothread
-pt ptMain;
-int MainThread(struct pt* pt){
-    PT_BEGIN(pt);
-    for(;;){
-      //TODO main thread execution
-    }
-    PT_END(pt);
-}
-*/
+//////
 
-//thread for interupting main thread and executing SLAM protocol
-/*
-pt ptSLAM;
-int SLAMThread(struct pt* pt){
-  PT_BEGIN(pt);
-    for(;;){
-      //TODO main thread execution
-    }
-  PT_END(pt);
-}
-*/
-///////
+printData pdt; 
 
-short completePercent;
+ThreadController tc = ThreadController();
 
-RPLidar l;
-RPLidar* L = &l;
-bool lstatus;
+Thread tmain = Thread();
+void tmainCB(){}
 
-Servo m1;
-Servo* M1 = &m1;
-bool m1status;
+Thread tlidar = Thread();
+void tlidarCB(){}
 
-Servo m2;
-Servo* M2 = &m2;
-bool m2status;
+Thread servo_m_1 = Thread();
+void m1CB(){}
 
-Hub hub;
-Hub* HUB = &hub;
-bool status;
+Thread servo_m_2 = Thread();
+void m2CB(){}
+
+
+RPLidar l; RPLidar* L = &l; bool lstatus;
+Servo m1; Servo* M1 = &m1; bool m1status;
+Servo m2; Servo* M2 = &m2; bool m2status;
+Hub hub; Hub* HUB = &hub; bool status;
 
 ///////
+
 void shutdown(Servo obj){if (&obj == &m1){m1status = false;}if (&obj == &m2){m2status = false;}}
 void shutdown(RPLidar obj){if (&obj == &l){lstatus = false;}} 
 void shutdown(Hub obj){if (&obj == &hub){status = false;}}
@@ -79,53 +89,59 @@ void servo_moduleCheck(Servo servo){
   //XXX test if comparing objects by runtime memory address works, alternative is to do nested classes, and the base class has the name
   if (&servo == &m1){
     if (m1.attached() == true){
-      cout << "Servo m1: OK";
+      cout << pdt.m1ok;
     }
     else{
-      cout << "Servo m1: ERR";
+      cout << pdt.m1er;
       shutdown(servo);
     }
   }
   if (&servo == &m2){
     if (m2.attached() == true){
-      cout << "Servo m2:" "OK";
+      cout << pdt.m2ok;
     }
     else{
-       cout << "Servo m2:" "ERR";
+       cout << pdt.m2er;
        shutdown(servo);
     }
   }
 }
 void boot_servo(vector<Servo> servolist){
   for (int i=0; (unsigned)i<servolist.size(); i++){
-    cout << "Booting Servo m" << i << " ...";
+    cout << pdt.sb << i << pdt.dotdotdot;
     servo_moduleCheck(servolist.at(i));
   };
 }
 
 void lidar_moduleCheck(RPLidar lidar){
   rplidar_response_device_info_t info;
-  if (IS_OK(lidar.getDeviceInfo(info, 100)) == true && IS_OK(lidar.waitPoint()) == true){cout<< "RPLidar l: OK";} //XXX check lidar variables
+  if (IS_OK(lidar.getDeviceInfo(info, 100)) == true && IS_OK(lidar.waitPoint()) == true){cout<< pdt.lok;} //XXX check lidar variables
   else{
-    cout << "RPLidar l: ERR"; 
+    cout << pdt.ler; 
     shutdown(lidar);
   }
 }
 void boot_lidar(RPLidar lidar){
-  cout << "Booting RPLidar...";
+  cout << pdt.lb;
   lidar_moduleCheck(lidar);
   //TODO object configuration
 }
 
+//////
 
 void setup() {
-  cout << "SETUP ///////";
-  cout << "Booting";
+  cout << pdt.SETUP;
+  cout << pdt.b;
+
+  Serial.begin(9600); //baud rate
+
   boot_hub(hub);
   boot_lidar(l);
   boot_servo({m1,m2});
+
+
 }
 
 void loop() {
-  
+  tc.run();
 }
